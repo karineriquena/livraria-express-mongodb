fcc1232f17a353147ee124992e59f43df237e373
Karine Riquena Limp <karine_limp@hotmail.com>
2023-03-09 15:58:49 -0300

    feat(server.js): adiciona rotas

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require('http');
 const port = 3000;
 
+const routes = {
+  '/': 'Curso de Node',
+  '/livros': 'Entrei na página de livros',
+  '/autores': 'Listagem de autores',
+  '/editoras': 'Página de editoras',
+  '/sobre': 'Informações sobre o projeto',
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Curso de Node');
+  res.end(routes[req.url]);
 });
 
 server.listen(port, () =>
