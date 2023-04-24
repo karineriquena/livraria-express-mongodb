a8330d4f902f15927f7084cee901cadb122d8bc8
Karine Riquena Limp <karine_limp@hotmail.com>
2023-04-24 18:42:33 -0300

    feat: add db connection

--- src/app_old.js
+++ src/app_old.js
@@ -1,8 +1,4 @@
 import express from 'express';
-import db from './config/dbConnect.js';
-
-db.on('error', console.log.bind(console, 'DB Connection Error!'));
-db.once('open', () => console.log('DB Connection Successful!'));
 
 const app = express();
 
